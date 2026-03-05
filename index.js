beb140b20484aed06a272816fe141a79d60c9d82
visal <2024063pov@aupp.edu.kh>
2026-03-05 21:35:53 +0700

    add put api

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.post('/', (req, res) => {
   res.send('Hell0, This is a FoodExpress server! POST');
 });
 
+app.put('/', (req, res) => {
+  res.send('Hell0, This is a FoodExpress server! PUT');
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
